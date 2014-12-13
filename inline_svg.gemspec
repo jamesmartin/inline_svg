af0b60ac1ad22ff3ab4d3d57df8cfae2c1dab1da
Ludwig Schubert <ludwig.schubert@qlearning.de>
2014-12-14 00:39:58 +0100

    Adds options for  and  nodes. Also adds (brittle) tests.

--- inline_svg.gemspec
+++ inline_svg.gemspec
@@ -20,6 +20,8 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "rspec", "~> 2.6"
+  spec.add_development_dependency "activesupport"
 
-  spec.add_runtime_dependency "nokogiri", "~> 1.6"
+  spec.add_runtime_dependency "nokogiri", "~> 1.6", '~> 1.6'
 end
--- lib/inline_svg/action_view/helpers.rb
+++ lib/inline_svg/action_view/helpers.rb
@@ -1,5 +1,5 @@
-require 'action_view/helpers'
-require 'action_view/context'
+require 'action_view/helpers' if defined?(Rails)
+require 'action_view/context' if defined?(Rails)
 
 module InlineSvg
   module ActionView
@@ -8,8 +8,18 @@ module InlineSvg
         file = File.read(Rails.root.join('app', 'assets', 'images', filename))
         doc = Nokogiri::HTML::DocumentFragment.parse file
         svg = doc.at_css 'svg'
-        if options[:class].present?
+        if options[:class]
           svg['class'] = options[:class]
+        end
+        if options[:title].present?
+          title = Nokogiri::XML::Node.new("title", doc)
+          title.content = options[:title]
+          svg.add_child title
+        end
+        if options[:desc].present?
+          desc = Nokogiri::XML::Node.new("desc", doc)
+          desc.content = options[:desc]
+          svg.add_child desc
         end
         doc.to_html.html_safe
       end
