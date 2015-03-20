bd9c2eccfabfbc9b095673c7ba22c56793219d40
James Martin <james.martin@betterbills.com>
2015-03-20 21:54:40 +1100

    Use Sprockets (or similar) to find asset files

--- lib/inline_svg/finds_assets.rb
+++
@@ -1,7 +0,0 @@
-module InlineSvg
-  class FindsAssets
-    def self.by_filename(filename)
-      Rails.root.join('app', 'assets', 'images', filename) if defined?(Rails)
-    end
-  end
-end
--- lib/inline_svg/railtie.rb
+++ lib/inline_svg/railtie.rb
@@ -5,6 +5,9 @@ module InlineSvg
       ActiveSupport.on_load :action_view do
         require "inline_svg/action_view/helpers"
         include InlineSvg::ActionView::Helpers
+        InlineSvg.configure do |config|
+          config.asset_finder = app.instance_variable_get(:@assets) # In most cases this will be the Sprockets::Environment instance of the Rails app.
+        end
       end
     end
   end
