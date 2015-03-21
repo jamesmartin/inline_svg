d141aba2b68ce9f2e83cb52a47315153c9303f1f
James Martin <james.martin@betterbills.com>
2015-03-22 08:33:01 +1100

    Refactor: Extract class to make helper method simpler to understand

--- lib/inline_svg/action_view/helpers.rb
+++ lib/inline_svg/action_view/helpers.rb
@@ -4,6 +4,29 @@ require 'nokogiri'
 require 'loofah'
 
 module InlineSvg
+  class SvgTransformPipeline
+    def self.generate_from(doc, options)
+      if options[:nocomment].present?
+        doc.scrub!(:strip)
+      end
+
+      svg = doc.at_css 'svg'
+      if options[:class]
+        svg['class'] = options[:class]
+      end
+
+      %i(title desc).each do |child|
+        if options[child].present?
+          node = Nokogiri::XML::Node.new(child.to_s, doc)
+          node.content = options[child]
+          svg.add_child node
+        end
+      end
+
+      doc
+    end
+  end
+
   module ActionView
     module Helpers
       def inline_svg(filename, options={})
@@ -13,23 +36,8 @@ module InlineSvg
           return "<svg><!-- SVG file not found: '#{filename}' --></svg>".html_safe
         end
 
-        if options[:nocomment].present?
-          doc.scrub!(:strip)
-        end
-
-        svg = doc.at_css 'svg'
-        if options[:class]
-          svg['class'] = options[:class]
-        end
-
-        %i(title desc).each do |child|
-          if options[child].present?
-            node = Nokogiri::XML::Node.new(child.to_s, doc)
-            node.content = options[child]
-            svg.add_child node
-          end
-        end
-
+        # Transforms based on options...
+        doc = SvgTransformPipeline.generate_from(doc, options)
         doc.to_html.html_safe
       end
     end
