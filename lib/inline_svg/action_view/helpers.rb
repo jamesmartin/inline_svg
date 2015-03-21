e5d04735e652993c7dba460037ca1629cfc62593
James Martin <james.martin@betterbills.com>
2015-03-22 09:52:15 +1100

    Refactor: Polymorphism replaces conditionals and pure functions replace mutation to reduce complexity

--- lib/inline_svg/action_view/helpers.rb
+++ lib/inline_svg/action_view/helpers.rb
@@ -4,41 +4,95 @@ require 'nokogiri'
 require 'loofah'
 
 module InlineSvg
-  class SvgTransformPipeline
-    def self.generate_from(doc, options)
-      if options[:nocomment].present?
-        doc.scrub!(:strip)
-      end
+  module TransformPipeline
+    module Transformations
+      class Transformation
+        def self.create_with_value(value)
+          self.new(value)
+        end
 
-      svg = doc.at_css 'svg'
-      if options[:class]
-        svg['class'] = options[:class]
-      end
+        attr_reader :value
 
-      %i(title desc).each do |child|
-        if options[child].present?
-          node = Nokogiri::XML::Node.new(child.to_s, doc)
-          node.content = options[child]
-          svg.add_child node
+        def initialize(value)
+          @value = value
+        end
+
+        def transform(*)
+          raise "#transform should be implemented by subclasses of #{self.class}"
         end
       end
 
-      doc
+      class NoComment < Transformation
+        def transform(doc)
+          doc = Loofah::HTML::DocumentFragment.parse(doc.to_html)
+          doc.scrub!(:strip)
+        end
+      end
+
+      class ClassAttribute < Transformation
+        def transform(doc)
+          doc = Nokogiri::XML::Document.parse(doc.to_html)
+          svg = doc.at_css 'svg'
+          svg['class'] = value
+          doc
+        end
+      end
+
+      class Title < Transformation
+        def transform(doc)
+          doc = Nokogiri::XML::Document.parse(doc.to_html)
+          node = Nokogiri::XML::Node.new('title', doc)
+          node.content = value
+          doc.at_css('svg').add_child(node)
+          doc
+        end
+      end
+
+      class Description < Transformation
+        def transform(doc)
+          doc = Nokogiri::XML::Document.parse(doc.to_html)
+          node = Nokogiri::XML::Node.new('desc', doc)
+          node.content = value
+          doc.at_css('svg').add_child(node)
+          doc
+        end
+      end
+
+      class NullTransformation < Transformation
+        def transform(doc)
+          doc
+        end
+      end
+
+      def self.all_transformations
+        {nocomment: NoComment, class: ClassAttribute, title: Title, desc: Description}
+      end
+
+      def self.lookup(transform_params)
+        transform_params.map do |key, value|
+          all_transformations.fetch(key, NullTransformation).create_with_value(value)
+        end
+      end
+    end
+
+    def self.generate_html_from(document, transform_params)
+      Transformations.lookup(transform_params).reduce(document) do |doc, transformer|
+        transformer.transform(doc)
+      end.to_html
     end
   end
 
   module ActionView
     module Helpers
-      def inline_svg(filename, options={})
+      def inline_svg(filename, transform_params={})
         begin
-          doc = Loofah::HTML::DocumentFragment.parse(AssetFile.named(filename))
+          doc = Nokogiri::XML::Document.parse(AssetFile.named(filename))
         rescue InlineSvg::AssetFile::FileNotFound
           return "<svg><!-- SVG file not found: '#{filename}' --></svg>".html_safe
         end
 
-        # Transforms based on options...
-        doc = SvgTransformPipeline.generate_from(doc, options)
-        doc.to_html.html_safe
+        doc = TransformPipeline.generate_html_from(doc, transform_params)
+        doc.html_safe
       end
     end
   end
