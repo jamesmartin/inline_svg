ad6cab08132ab1605296f9b26809e4c95a631f82
James Martin <james.martin@betterbills.com>
2015-03-22 22:41:52 +1100

    We can use the Size transform and have confidence in its lookup

--- lib/inline_svg/transform_pipeline/transformations.rb
+++ lib/inline_svg/transform_pipeline/transformations.rb
@@ -1,12 +1,16 @@
 module InlineSvg::TransformPipeline::Transformations
   def self.all_transformations
-    {nocomment: NoComment, class: ClassAttribute, title: Title, desc: Description}
+    {nocomment: NoComment, class: ClassAttribute, title: Title, desc: Description, size: Size}
   end
 
   def self.lookup(transform_params)
-    transform_params.map do |key, value|
+    without_empty_values(transform_params).map do |key, value|
       all_transformations.fetch(key, NullTransformation).create_with_value(value)
     end
+  end
+
+  def self.without_empty_values(params)
+    params.reject {|key, value| value.nil?}
   end
 end
 
---
+++ spec/transformation_pipeline/transformations_spec.rb
@@ -0,0 +1,41 @@
+require 'inline_svg/transform_pipeline'
+
+describe InlineSvg::TransformPipeline::Transformations do
+  context "looking up transformations" do
+    it "returns configured transformations when parameters are supplied" do
+      transformations = InlineSvg::TransformPipeline::Transformations.lookup(
+        nocomment: 'irrelevant',
+        class: 'irrelevant',
+        title: 'irrelevant',
+        desc: 'irrelevant',
+        size: 'irrelevant'
+      )
+
+      expect(transformations.map(&:class)).to match_array([
+        InlineSvg::TransformPipeline::Transformations::NoComment,
+        InlineSvg::TransformPipeline::Transformations::ClassAttribute,
+        InlineSvg::TransformPipeline::Transformations::Title,
+        InlineSvg::TransformPipeline::Transformations::Description,
+        InlineSvg::TransformPipeline::Transformations::Size
+      ])
+    end
+
+    it "returns a benign transformation when asked for an unknown transform" do
+      transformations = InlineSvg::TransformPipeline::Transformations.lookup(
+        not_a_real_transform: 'irrelevant'
+      )
+
+      expect(transformations.map(&:class)).to match_array([
+        InlineSvg::TransformPipeline::Transformations::NullTransformation
+      ])
+    end
+
+    it "does not return a transformation when a value is not supplied" do
+      transformations = InlineSvg::TransformPipeline::Transformations.lookup(
+        title: nil
+      )
+
+      expect(transformations.map(&:class)).to match_array([])
+    end
+  end
+end
