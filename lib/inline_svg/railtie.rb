9943675433f52b30d51cb981019887889bc357a3
James Martin <james.martin@betterbills.com>
2015-04-03 13:44:10 +1100

    Support Sprockets::Asset for versions 2 and 3 of Sprockets

--- lib/inline_svg/railtie.rb
+++ lib/inline_svg/railtie.rb
@@ -5,9 +5,14 @@ module InlineSvg
       ActiveSupport.on_load :action_view do
         require "inline_svg/action_view/helpers"
         include InlineSvg::ActionView::Helpers
-        InlineSvg.configure do |config|
-          config.asset_finder = app.instance_variable_get(:@assets) # In most cases this will be the Sprockets::Environment instance of the Rails app.
-        end
+      end
+    end
+
+    config.after_initialize do |app|
+      InlineSvg.configure do |config|
+        # In default Rails apps, this will be a fully operational
+        # Sprockets::Environment instance
+        config.asset_finder = app.instance_variable_get(:@assets)
       end
     end
   end
