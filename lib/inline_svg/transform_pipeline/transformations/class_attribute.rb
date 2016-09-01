b90b436a3180ca7ff2e06da6625f0a0252958892
James Martin <jamesmartin@github.com>
2016-09-01 11:23:30 +1000

    Preserve existing classes on the SVG document

--- lib/inline_svg/transform_pipeline/transformations/class_attribute.rb
+++ lib/inline_svg/transform_pipeline/transformations/class_attribute.rb
@@ -2,8 +2,10 @@ module InlineSvg::TransformPipeline::Transformations
   class ClassAttribute < Transformation
     def transform(doc)
       doc = Nokogiri::XML::Document.parse(doc.to_html)
-      svg = doc.at_css 'svg'
-      svg['class'] = value
+      svg = doc.at_css "svg"
+      classes = (svg["class"] || "").split(" ")
+      classes << value
+      svg["class"] = classes.join(" ")
       doc
     end
   end
