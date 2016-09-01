84efd10e462a5bf930e48ab91566d998d905172d
James Martin <jamesmartin@github.com>
2016-09-01 11:14:43 +1000

    Test for basic class attribute transform

---
+++ spec/transformation_pipeline/transformations/class_attribute_spec.rb
@@ -0,0 +1,12 @@
+require 'inline_svg/transform_pipeline'
+
+describe InlineSvg::TransformPipeline::Transformations::ClassAttribute do
+  it "adds a class attribute to a SVG document" do
+    document = Nokogiri::XML::Document.parse('<svg>Some document</svg>')
+    transformation = InlineSvg::TransformPipeline::Transformations::ClassAttribute.create_with_value("some-class")
+
+    expect(transformation.transform(document).to_html).to eq(
+      "<svg class=\"some-class\">Some document</svg>\n"
+    )
+  end
+end
