dcda87e05846ef2b7a78f9f27bea8a9a6e41be49
James Martin <jamesmartin@github.com>
2016-11-22 14:11:15 +1100

    Dasherize data attribute names

--- spec/transformation_pipeline/transformations/data_attributes_spec.rb
+++ spec/transformation_pipeline/transformations/data_attributes_spec.rb
@@ -19,6 +19,15 @@ describe InlineSvg::TransformPipeline::Transformations::DataAttributes do
     )
   end
 
+  it "dasherizes a data attribute name with multiple parts" do
+    document = Nokogiri::XML::Document.parse('<svg>Some document</svg>')
+    transformation = InlineSvg::TransformPipeline::Transformations::DataAttributes.create_with_value({some_other_name: "value"})
+
+    expect(transformation.transform(document).to_html).to eq(
+      "<svg data-some-other-name=\"value\">Some document</svg>\n"
+    )
+  end
+
   context "when multiple data attributes are supplied" do
     it "adds data attributes to the SVG for each supplied value" do
       document = Nokogiri::XML::Document.parse('<svg>Some document</svg>')
