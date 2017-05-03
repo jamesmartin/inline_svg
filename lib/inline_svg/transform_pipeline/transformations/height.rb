221822158d0e223d57cff1c2bf01b9beed56089e
James Martin <jamesmartin@github.com>
2017-05-03 17:40:52 +1000

    Handles documents without SVG root elements

--- lib/inline_svg/transform_pipeline/transformations/height.rb
+++ lib/inline_svg/transform_pipeline/transformations/height.rb
@@ -1,10 +1,9 @@
 module InlineSvg::TransformPipeline::Transformations
   class Height < Transformation
     def transform(doc)
-      doc = Nokogiri::XML::Document.parse(doc.to_html)
-      svg = doc.at_css 'svg'
-      svg['height'] = self.value
-      doc
+      with_svg(doc) do |svg|
+        svg["height"] = self.value
+      end
     end
   end
 end
