e505f86682b945ec94d55753353af52e92a2cef8
James Martin <jamesmartin@github.com>
2017-05-03 17:51:53 +1000

    Handle documents without SVG root elements

--- lib/inline_svg/transform_pipeline/transformations/id_attribute.rb
+++ lib/inline_svg/transform_pipeline/transformations/id_attribute.rb
@@ -1,10 +1,9 @@
 module InlineSvg::TransformPipeline::Transformations
   class IdAttribute < Transformation
     def transform(doc)
-      doc = Nokogiri::XML::Document.parse(doc.to_html)
-      svg = doc.at_css 'svg'
-      svg['id'] = self.value
-      doc
+      with_svg(doc) do |svg|
+        svg["id"] = self.value
+      end
     end
   end
 end
