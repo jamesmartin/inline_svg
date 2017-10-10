51fbae79199c27eb876a9f0d3f8556cf67d58015
James Martin <jamesmartin@github.com>
2017-10-11 07:05:01 +1100

    Add braces to method definitions

--- lib/inline_svg/io_resource.rb
+++ lib/inline_svg/io_resource.rb
@@ -1,17 +1,17 @@
 module InlineSvg
   module IOResource
-    def self.=== object
+    def self.===(object)
       object.is_a?(IO) || object.is_a?(StringIO)
     end
 
-    def self.default_for object
+    def self.default_for(object)
       case object
       when StringIO then ''
       when IO then 1
       end
     end
 
-    def self.read object
+    def self.read(object)
       start = object.pos
       str = object.read
       object.seek start
