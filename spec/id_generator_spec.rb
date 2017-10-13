b17ac2e73dc6304d6f766b3ff1f8110707d2f728
James Martin <jamesmartin@github.com>
2017-10-13 17:10:06 +1100

    Fail: should generate IDs with randomness

--- spec/id_generator_spec.rb
+++ spec/id_generator_spec.rb
@@ -1,8 +1,10 @@
 require_relative '../lib/inline_svg/id_generator'
 
 describe InlineSvg::IdGenerator do
-  it "generates a hexencoded ID based on a salt" do
-    expect(InlineSvg::IdGenerator.generate("some-base", "some-salt")).
-      to eq("ksiuuy1jduycacqpoj5smn2kyt9iv02")
+  it "generates a hexencoded ID based on a salt and a random value" do
+    randomizer = -> { "some-random-value" }
+
+    expect(InlineSvg::IdGenerator.generate("some-base", "some-salt", randomness: randomizer)).
+      to eq("t2c17mkqnvopy36iccxspura7wnreqf")
   end
 end
