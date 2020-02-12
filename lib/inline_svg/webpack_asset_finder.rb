0477fec3dd58b0cc6783d1a60c61b55ed372a77e
Kyle Fox <kylefox@gmail.com>
2020-02-12 13:30:33 -0700

    break network request and tempfile construction into different private methods

--- lib/inline_svg/webpack_asset_finder.rb
+++ lib/inline_svg/webpack_asset_finder.rb
@@ -13,28 +13,38 @@ module InlineSvg
       return unless file_path
 
       if Webpacker.dev_server.running?
-        http = Net::HTTP.new(Webpacker.dev_server.host, Webpacker.dev_server.port)
-        http.use_ssl = Webpacker.dev_server.https?
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        asset = http.request(Net::HTTP::Get.new(file_path)).body
-
-        begin
-          Tempfile.new(file_path).tap do |file|
-            file.binmode
-            file.write(asset)
-            file.rewind
-          end
-        rescue StandardError => e
-          Rails.logger.error "Error creating tempfile: #{e}"
-          raise
-        end
-
+        dev_server_asset(file_path)
       else
         public_path = Webpacker.config.public_path
         return unless public_path
 
         File.join(public_path, file_path)
       end
+    end
+
+    private
+
+    def dev_server_asset(file_path)
+      asset = fetch_from_dev_server(file_path)
+
+      begin
+        Tempfile.new(file_path).tap do |file|
+          file.binmode
+          file.write(asset)
+          file.rewind
+        end
+      rescue StandardError => e
+        Rails.logger.error "Error creating tempfile: #{e}"
+        raise
+      end
+    end
+
+    def fetch_from_dev_server(file_path)
+      http = Net::HTTP.new(Webpacker.dev_server.host, Webpacker.dev_server.port)
+      http.use_ssl = Webpacker.dev_server.https?
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      http.request(Net::HTTP::Get.new(file_path)).body
     end
   end
 end
