944be145f5fe31f7239209bf279e556e8658be36
Martín Zamuner <martinzamuner@gmail.com>
2021-12-08 14:48:09 +0100

    Add support for Propshaft

--- spec/finds_asset_paths_spec.rb
+++ spec/finds_asset_paths_spec.rb
@@ -46,6 +46,21 @@ describe InlineSvg::FindsAssetPaths do
     end
   end
 
+  context "when propshaft finder returns an object which supports only the pathname method" do
+    it "returns fully qualified file paths from Propshaft" do
+      propshaft = double('PropshaftDouble')
+
+      expect(propshaft).to receive(:find_asset).with('some-file').
+        and_return(double(pathname: Pathname('/full/path/to/some-file')))
+
+      InlineSvg.configure do |config|
+        config.asset_finder = propshaft
+      end
+
+      expect(InlineSvg::FindsAssetPaths.by_filename('some-file')).to eq Pathname('/full/path/to/some-file')
+    end
+  end
+
   context "when webpack finder returns an object with a relative asset path" do
     it "returns the fully qualified file path" do
       webpacker = double('WebpackerDouble')
