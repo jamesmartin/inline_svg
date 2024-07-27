a9ab1b00eae89bdb8a0fc6b08b3fc6fe43d2db6c
Geremia Taglialatela <tagliala.dev@gmail.com>
2024-07-27 10:05:50 +0200

    Add support to Shakapacker
    Shakapacker is the official, actively maintained successor to Webpacker.
    
    Shakapacker v7 changed the spelling of `Webpacker` to `Shakapacker` in
    the entire project, but still provided backward compatibility for
    `Webpacker` spelling.
    
    v8 dropped the deprecated spelling
    
    This commit also:
    - Checks if `Shakapacker` is defined; if not, it falls back on
      `Webpacker`.
    - Uses the scope resolution operator to resolve at top-level
      scope
    - Checks `protocol` instead of `https?` because the former is available
      from Webpacker 3 and the latter is not available anymore in
      Shakapacker >= 8
    
    Refs:
    - shakacode/shakapacker#414
    - shakacode/shakapacker#429
    - shakacode/shakapacker#486
    
    Close #156

--- spec/webpack_asset_finder_spec.rb
+++ spec/webpack_asset_finder_spec.rb
@@ -10,4 +10,14 @@ describe InlineSvg::WebpackAssetFinder do
       expect(described_class.find_asset('some-file').pathname).to be_nil
     end
   end
+
+  context "when Shakapacker is defined" do
+    it "uses the new spelling" do
+      stub_const('Rails', double('Rails').as_null_object)
+      stub_const('Shakapacker', double('Shakapacker').as_null_object)
+      expect(::Shakapacker.manifest).to receive(:lookup).with('some-file').and_return(nil)
+
+      expect(described_class.find_asset('some-file').pathname).to be_nil
+    end
+  end
 end
