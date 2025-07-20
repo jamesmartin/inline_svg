ea523f3314f9ad39858e822065a1dbc3dedaca1f
Geremia Taglialatela <tagliala.dev@gmail.com>
2025-07-20 20:33:27 +0200

    Remove unused `IOResource.default_for` method
    This method, introduced in the 4b13f77 commit along with the IOResource
    class, is not utilized in either production or test code.
    
    To the best of my understanding, this discrepancy may be attributed to
    a copy-and-paste error.
    
    Ref: #186

--- lib/inline_svg/io_resource.rb
+++ lib/inline_svg/io_resource.rb
@@ -6,13 +6,6 @@ module InlineSvg
       object.is_a?(IO) || object.is_a?(StringIO)
     end
 
-    def self.default_for(object)
-      case object
-      when StringIO then ''
-      when IO then 1
-      end
-    end
-
     def self.read(object)
       start = object.pos
       str = object.read
